1035fc054870da8703db04e3fb167eff3a6470af
Jon Lindell <jhlindell@yahoo.com>
2017-05-25 09:59:09 -0600

    add api route and 404 if bad route

--- server.js
+++ server.js
@@ -3,7 +3,13 @@ const app = express();
 
 app.use(express.static("./public"));
 
-
+app.get("/:id", function(req, res){
+  if(req.params.id === "api"){
+    res.send("Hello from API");
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 app.listen(8000, function(){
   console.log("listening on port 8000");
